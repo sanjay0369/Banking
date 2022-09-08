09301a75399f6390d0ea22ca4365cb54aa46b227
sanjay0369 <112961141+sanjay0369@users.noreply.github.com>
2022-09-08 22:11:28 +0530

    loginpage
    created loginpage

---
+++ BankingProject/src/main/java/com/qa/pageLayer/LoginPage.java
@@ -0,0 +1,22 @@
+package com.qa.pageLayer;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage 
+{
+  
+   public LoginPage (WebDriver driver)
+   {
+	   PageFactory.initElements(driver, this);
+   }
+   
+   @FindBy (xpath = "(//a[@class='btn btn-color'])[1]")
+   private WebElement login_page;
+   public void clickOnLogin ()
+   {
+	   login_page.click();
+   }
+}
